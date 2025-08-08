b4d335e3d92c85c0ad1dd1e02dde7635afa64f4a
Tuavsn <hoctuavsn@gmail.com>
2025-08-08 23:50:25 +0700

    Setup project

---
+++ src/main/java/com/trinhhoctuan/articlecheck/ArticlecheckApplication.java
@@ -0,0 +1,13 @@
+package com.trinhhoctuan.articlecheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlecheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticlecheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trinhhoctuan/articlecheck/ArticlecheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trinhhoctuan.articlecheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlecheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
