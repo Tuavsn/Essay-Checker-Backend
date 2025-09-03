9a93bd28852a037f0b2a6f9f610d01708a2d390c
Tuavsn <hoctuavsn@gmail.com>
2025-09-04 00:04:48 +0700

    feat: upgrade JDK from 17 to 24

--- src/main/java/com/trinhhoctuan/articlecheck/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.trinhhoctuan.articlecheck.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class AppConfig implements WebMvcConfigurer {
-
-}
